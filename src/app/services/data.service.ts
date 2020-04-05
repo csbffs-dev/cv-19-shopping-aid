a726b6264e273cdb67415fcf562156ff36a5f483
Paul S <tsathira19@gmail.com>
2020-04-05 18:28:19 -0500

    change proxy config to JS and add prod environment var

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,31 +1,39 @@
-import { Injectable } from '@angular/core';
+import { Injectable, isDevMode } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, Subject } from 'rxjs';
+import { Subject } from 'rxjs';
 import { Store } from '../models/store';
 import { Address } from '../models/address';
 import { User } from '../models/user';
+import { SERVER_URL } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 // Service class for CV-19 Shopping Aid API Calls
 export class DataService {
-  readonly SERVER_URL = 'api';
+  private serverUrl: string;
   readonly GET_USER = '/user/query';
   readonly NEW_USER = '/user/setup';
   storesData = new Subject<Store[]>();
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { 
+    this.serverUrl = isDevMode()? 'api': SERVER_URL;
+    if(!isDevMode()) {
+      console.log('not in dev mode');
+    } else {
+      console.log('in dev mode');
+    }
+  }
 
   getUser(userId: string) {
     const config = { headers: new HttpHeaders().set('Content-Type', 'application/json') };
     const data = { 'user_id': userId };
-    return this.http.post(this.SERVER_URL + this.GET_USER, data, config);
+    return this.http.post(this.serverUrl + this.GET_USER, data, config);
   }
 
   signUpNewUser(user: User) {
     const config = { headers: new HttpHeaders().set('Content-Type', 'application/json') };
     const data = { 'first_name': user.firstName, 'last_name' : user.lastName, 'zip_code' : user.zipCode }
-    return this.http.post<any>(this.SERVER_URL + this.NEW_USER, data, config);
+    return this.http.post<any>(this.serverUrl + this.NEW_USER, data, config);
   }
 
   getStores(storeName: string, zipCode: string): Subject<Store[]> {
