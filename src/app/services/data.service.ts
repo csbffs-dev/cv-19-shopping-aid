681bbd232ccb90695ffee6e32b82f181e9f57db4
stytch16 <stan.yt.chen@gmail.com>
2020-04-08 03:14:17 -0700

    Add fixes to get the ios simulator working with localhost.
    $ ionic cap sync
    $ ionic cap run ios // no need for --prod, both dev and prod server urls are set to http://localhost:8080
    
    The changes in android/ were needed to get the app running on Android emulator.
    However the app crashes once it opens in Android. Need to root cause and fix.

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -16,9 +16,9 @@ export class DataService {
   readonly EDIT_USER = '/user/edit';
   readonly ADD_STORE = '/store/add';
   storesData = new Subject<Store[]>();
-  constructor(private http: HttpClient) { 
-    this.serverUrl = isDevMode()? 'api': SERVER_URL;
-    if(!isDevMode()) {
+  constructor(private http: HttpClient) {
+    this.serverUrl = SERVER_URL;
+    if (!isDevMode()) {
       console.log('not in dev mode');
     } else {
       console.log('in dev mode');
@@ -33,13 +33,13 @@ export class DataService {
 
   signUpNewUser(user: User) {
     const config = { headers: new HttpHeaders().set('Content-Type', 'application/json') };
-    const data = { 'first_name': user.firstName, 'last_name' : user.lastName, 'zip_code' : user.zipCode }
+    const data = { 'first_name': user.firstName, 'last_name': user.lastName, 'zip_code': user.zipCode }
     return this.http.post<any>(this.serverUrl + this.NEW_USER, data, config);
   }
 
   updateUserInfo(user: User) {
     const config = { headers: new HttpHeaders().set('Content-Type', 'application/json') };
-    const data = { 'user_id': user.userId, 'first_name': user.firstName, 'last_name' : user.lastName, 'zip_code' : user.zipCode }
+    const data = { 'user_id': user.userId, 'first_name': user.firstName, 'last_name': user.lastName, 'zip_code': user.zipCode }
     return this.http.post<any>(this.serverUrl + this.EDIT_USER, data, config);
   }
 
